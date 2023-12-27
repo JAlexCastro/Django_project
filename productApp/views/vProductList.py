b0ecef92d65d9bf7cc456a2948f9da3c1ac7b133
Alejandro Castro <jalejandro.ecom@gmail.com>
2023-12-26 21:08:15 -0300

    Listado de productos

--- productApp/views/vProductList.py
+++ productApp/views/vProductList.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from ..models import Product
 
 # Create your views here.
 def productList(request):
-    return render(request, "productList.html")
+    
+    productos = Product.objects.all()[:9]
+    return render(request, "productList.html", {'productos' : productos})
 
 
 
